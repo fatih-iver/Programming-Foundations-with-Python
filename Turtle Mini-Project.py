c1a8e6b32b3b7839a0519f9b16081ea73423652a
Fatih İver <fatih.iver@boun.edu.tr>
2018-04-03 18:49:57 +0300

    Add files via upload

---
+++ Turtle Mini-Project.py
@@ -0,0 +1,30 @@
+import turtle, random
+
+colors  = ["red","green","blue","orange","purple","pink","yellow"]
+
+def draw_triangle(Turtle):
+    
+    for i in range(3):
+        color = random.choice(colors)
+        Turtle.color(color)
+        Turtle.forward(100)
+        Turtle.left(120)
+        
+Screen = turtle.Screen()
+Screen.bgcolor("black")
+        
+Turtle = turtle.Turtle()
+Turtle.speed(0)
+Turtle.pensize(1)
+Turtle.shape("turtle")
+Turtle.color("green")
+
+for i in range(24):
+    rotate = i * 15
+    Turtle.home()
+    Turtle.left(rotate)
+    Turtle.color("white")
+    Turtle.forward(50)
+    draw_triangle(Turtle)
+    
+Screen.exitonclick()
