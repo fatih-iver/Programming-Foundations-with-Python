cea6bb6f18b22696b7cad6294aa754d57e421141
Fatih İver <fatih.iver@boun.edu.tr>
2018-04-09 10:52:38 +0300

    Add files via upload

--- Check Profanity.py
+++ Check Profanity.py
@@ -1,5 +1,18 @@
-quotes = open("C:\Users\fatih\Desktop\dummy.txt")
-content = quotes.read()
-print(content)
-quotes.close()
+import urllib.request
 
+def read_text():
+    quotes = open("C:\\Users\\fatih\\Desktop\\dummy.txt")
+    content = quotes.read()
+    check_profanity(content)
+    quotes.close()
+    
+    
+def check_profanity(text_to_check):
+    print(text_to_check)
+    text_to_check = "-".join(text_to_check.split())    
+    connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+    
+read_text()
